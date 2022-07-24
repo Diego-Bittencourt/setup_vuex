84e4c620162fe517133e30502ffdeea232f794b8
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-24 16:37:06 +0900

    TO access the vuex, useStore() must be imported from vuex and used.
    The useStore can be used with getter, actions, mutations and other
    functions from vuex.

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script>
+import { useStore } from 'vuex';
 export default {
   setup() {
-    function inc() {}
+    const store = useStore();
+    //we got access to the store by using the useStore() function inside the store();
+    //
+    function inc() {
+      store.dispatch('increment');
+    }
 
     return { inc };
   },
